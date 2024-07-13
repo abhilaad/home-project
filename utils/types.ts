ddfc59b2fe33f5e02db71d4344a733097d643f9f
abhilaad <abhishek.24042013@gmail.com>
2024-07-13 16:52:50 +0530

    property card

--- utils/types.ts
+++ utils/types.ts
@@ -3,4 +3,11 @@ export type actionFunction = (
   formData: FormData
 ) => Promise<{ message: string }>;
 
-
+export type PropertyCardProps = {
+  image: string;
+  id: string;
+  name: string;
+  tagline: string;
+  country: string;
+  price: number;
+};
