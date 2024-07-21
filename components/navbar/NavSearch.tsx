b142d15401269468e8df4a1ee3945d1ebd0497ea
abhilaad <abhishek.24042013@gmail.com>
2024-07-21 22:21:25 +0530

    reservation and admin page

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,11 @@
 'use client';
 import { Input } from '../ui/input';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { useState, useEffect } from 'react';
 
 function NavSearch() {
   const searchParams = useSearchParams();
-
-  const pathname = usePathname();
   const { replace } = useRouter();
   const [search, setSearch] = useState(
     searchParams.get('search')?.toString() || ''
@@ -19,8 +17,9 @@ function NavSearch() {
     } else {
       params.delete('search');
     }
-    replace(`${pathname}?${params.toString()}`);
-  }, 300);
+    replace(`/?${params.toString()}`);
+  }, 500);
+
   useEffect(() => {
     if (!searchParams.get('search')) {
       setSearch('');
@@ -28,7 +27,7 @@ function NavSearch() {
   }, [searchParams.get('search')]);
   return (
     <Input
-      type='search'
+      type='text'
       placeholder='find a property...'
       className='max-w-xs dark:bg-muted '
       onChange={(e) => {
